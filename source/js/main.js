469aa9555c27bf518abfde9f2b5d00e27597579f
roman <romasaab@gmail.com>
2023-11-21 19:24:40 +0200

    remove styles

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,47 @@
-import './_components';
+import Swiper from './vendor/swiper';
+
+const slider = document.querySelector('.nw-benefits-slider__container');
+
+if(slider){
+  const swiper = new Swiper(slider, {  
+    slidesPerView: 3,  
+    spaceBetween: 20,
+    observer: true,
+    observeParents: true,
+    loop:true,
+    centeredSlides: true,
+    speed: 1000,
+
+    navigation: {
+      prevEl:'.nw-benefits-slider__btn-prev',
+      nextEl: '.nw-benefits-slider__btn-next',
+    },
+    
+ 
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        autoplay: false,
+      },
+
+      576: {
+        slidesPerView: 1.5,
+        autoplay:{
+          delay: 4000,
+        },
+      },
+
+      1240: { 
+        slidesPerView: 2.4,
+      },
+
+      1920: { 
+        slidesPerView: 2.6,
+      },
+
+      2200: { 
+        slidesPerView: 3,
+      },
+    },
+  });
+  }
